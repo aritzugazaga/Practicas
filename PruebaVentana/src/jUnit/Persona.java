3b9e92ac0c65b054375ad9fc706f09262933739a
Aritz Zugazaga <aritzugazaga@opendeusto.es>
2019-10-04 13:37:32 +0200

    Test persona creado

--- PruebaVentana/src/jUnit/Persona.java
+++ PruebaVentana/src/jUnit/Persona.java
@@ -1,5 +1,23 @@
 package jUnit;
 
 public class Persona {
+	String nombre;
+	String apellido;
+	
+	public Persona (String nombre, String apellido) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public String getNombreCompleto() {
+		return apellido + ", " + nombre;
+	}
 }
---
+++ PruebaVentana/test/PersonaTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import jUnit.Persona;
+
+public class PersonaTest {
+
+	@Test
+	public void testGetNombre() {
+		Persona p = new Persona("Unai", "Aguilera");
+		assertEquals("Unai", p.getNombre());
+	}
+	
+	@Test
+	public void testGetApellido() {
+		Persona p = new Persona("Unai", "Aguilera");
+		assertEquals("Aguilera", p.getApellido());
+	}
+	
+	@Test
+	public void testGetNombreCompleto() {
+		Persona p = new Persona("Unai", "Aguilera");
+		assertEquals("Aguilera, Unai", p.getNombreCompleto());
+	}
+
+}
