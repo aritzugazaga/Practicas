98bad766ca94ecb2b99d6d12a1141908bf3c25f8
Aritz Zugazaga <aritzugazaga@opendeusto.es>
2019-10-08 09:04:12 +0200

    Before

--- PruebaVentana/test/CalculadoraImplTest.java
+++ PruebaVentana/test/CalculadoraImplTest.java
@@ -1,32 +1,36 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import jUnit.CalculadoraImpl;
 
 public class CalculadoraImplTest {
+	
+	private CalculadoraImpl c;
+	
+	@Before
+	public void setUp() {
+		c = new CalculadoraImpl();
+	}
 
 	@Test
 	public void testSumar() {
-		CalculadoraImpl c = new CalculadoraImpl();
 		assertEquals(5, c.Sumar(2, 3));
 	}
 	
 	@Test
 	public void testRestar() {
-		CalculadoraImpl c = new CalculadoraImpl();
 		assertEquals(1, c.Restar(3, 2));
 	}
 	
 	@Test
 	public void testMultiplicar() {
-		CalculadoraImpl c = new CalculadoraImpl();
 		assertEquals(6, c.Multiplicar(2, 3));
 	}
 	
 	@Test
 	public void testEsPar() {
-		CalculadoraImpl c = new CalculadoraImpl();
 		assertEquals(false, c.esPar(5));
 		assertEquals(true, c.esPar(4));
 	}
