abe701246ed8d22fd5a30d728e344a98ca7c5dd2
Aritz Zugazaga <aritzugazaga@opendeusto.es>
2019-10-22 09:22:18 +0200

    sql ini

---
+++ lite/src/ejemplo/Ejemplo.java
@@ -0,0 +1,32 @@
+package ejemplo;
+
+import java.sql.*;
+
+public class Ejemplo {
+	
+	public static void main(String[] args) {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:data/ejemplo.db");
+			
+			Statement stmt = conn.createStatement();
+			//conexión abierta
+			//statement creado
+			
+			// consultar datos
+			stmt.executeQuery("SELECT nombre, apellido FROM usuario");
+			
+			//ya no queremos usarlos mas
+			stmt.close();
+			conn.close();
+			
+		}catch (ClassNotFoundException e) {
+			System.out.println("No se ha podido guardar el driver");
+		} catch (SQLException e) {
+			System.out.println("No se ha podido conectar con la BD");
+		}
+		
+		
+	}
+}
